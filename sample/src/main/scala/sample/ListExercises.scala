98e83de0f1576c07df0acaf1fc8dbac2dfd7fd1f
sanjiv sahayam <sanjsmailbox@gmail.com>
2018-05-30 22:01:07 +1000

    comment implementation

--- sample/src/main/scala/sample/ListExercises.scala
+++ sample/src/main/scala/sample/ListExercises.scala
@@ -2,12 +2,13 @@ package sample
 
 object ListExercises {
 
-  def prepend[A](value: A, list: List[A]): List[A] = value :: list
+  def prepend[A](value: A, list: List[A]): List[A] = ???//value :: list
 
-  def append[A](list: List[A], value: A): List[A] = list :+ value
+  def append[A](list: List[A], value: A): List[A] = ???//list :+ value
 
-  def isEmpty[A](list: List[A]): Boolean = list match {
-    case Nil => true
-    case _   => false
-  }
+  def isEmpty[A](list: List[A]): Boolean = ???
+  // list match {
+  //   case Nil => true
+  //   case _   => false
+  // }
 }
